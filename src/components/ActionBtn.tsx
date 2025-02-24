3aecf11f36eff12521b24b7ce861598cc018870b
James Wylie <9528193+jameswylie@users.noreply.github.com>
2025-02-24 19:00:56 +0000

    feat: allow disabled button.

--- src/components/ActionBtn.tsx
+++ src/components/ActionBtn.tsx
@@ -3,17 +3,22 @@ import { useState } from "react";
 export const ActionBtn = ({
   text,
   onClick,
+  disabled = false,
 }: {
   text: string;
   onClick: Function;
+  disabled?: boolean;
 }) => {
-
   const [loading, setLoading] = useState(false);
 
   return (
-    <button id="action-button" onClick={() => onClick()} className={loading ? "loading" : ""}>
+    <button
+      id="action-button"
+      onClick={() => onClick()}
+      className={loading ? "loading" : ""}
+      disabled={disabled}
+    >
       {text}
     </button>
   );
-
 };
